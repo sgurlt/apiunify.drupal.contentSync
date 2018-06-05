209a2ee3187ae2b44a69728676a1ca4e7717df27
Sebastian Gurlt <gurlt@brightsolutions.de>
2018-06-05 09:01:21 +0200

    Allow video_embed_field fields.

--- src/Plugin/drupal_content_sync/field_handler/DefaultFieldHandler.php
+++ src/Plugin/drupal_content_sync/field_handler/DefaultFieldHandler.php
@@ -44,6 +44,7 @@ class DefaultFieldHandler extends FieldHandlerBase {
       "list_float",
       "list_integer",
       "viewfield",
+      "video_embed_field",
     ];
     return in_array($field->getType(), $allowed) !== FALSE;
   }
